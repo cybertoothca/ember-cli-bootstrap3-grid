6b07eb6ae4ec7f63973be17b5a900d0e75c2db26
nadnoslen <dan@cybertooth.io>
2020-01-15 19:14:45 -0700

    v2.13.3...v2.14.2
    # Conflicts:
    #	.gitignore
    #	ember-cli-build.js
    #	testem.js
    #	tests/dummy/config/environment.js

--- testem.js
+++ testem.js
@@ -11,12 +11,12 @@ module.exports = {
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
+        // process.env.CI ? '--no-sandbox' : null,
         '--headless',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
+        // '--disable-gpu',
+        // '--disable-dev-shm-usage',
         // '--disable-software-rasterizer',
-        '--mute-audio',
+        // '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ].filter(Boolean)

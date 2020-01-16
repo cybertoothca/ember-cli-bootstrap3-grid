a0c770f4bc8d8f79799eb55b0dc953ee3e553f37
nadnoslen <dan@cybertooth.io>
2020-01-15 19:17:11 -0700

    v2.14.2...v2.15.1
    # Conflicts:
    #	testem.js
    #	tests/dummy/app/index.html

--- testem.js
+++ testem.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
@@ -8,19 +9,11 @@ module.exports = {
     'Chrome'
   ],
   browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        // process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        // '--disable-gpu',
-        // '--disable-dev-shm-usage',
-        // '--disable-software-rasterizer',
-        // '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ].filter(Boolean)
-    }
-  },
-  browser_start_timeout: 60 // for github actions, seems timeouts are happening
+    Chrome: [
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 };

50b1ca976f46ede452bce0786df92c8d6813abd4
nadnoslen <dan@cybertooth.io>
2020-01-18 14:19:38 -0800

    qunit codemod

--- tests/unit/mixins/viewport-test.js
+++ tests/unit/mixins/viewport-test.js
@@ -2,62 +2,62 @@ import EmberObject from '@ember/object';
 import ViewportMixin from 'ember-cli-bootstrap3-grid/mixins/viewport';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | viewport');
+module('Unit | Mixin | viewport', function() {
+  test('it works', function(assert) {
+    let ViewportObject = EmberObject.extend(ViewportMixin);
+    let subject = ViewportObject.create();
+    assert.ok(subject);
+  });
 
-test('it works', function(assert) {
-  let ViewportObject = EmberObject.extend(ViewportMixin);
-  let subject = ViewportObject.create();
-  assert.ok(subject);
-});
+  test('when xs?', function (assert) {
+    let ViewportObject = EmberObject.extend(ViewportMixin);
+    const viewport = ViewportObject.create({ '_width': 767 });
+    assert.ok(viewport.get('xs?'), 'Should be xs');
+    assert.ok(viewport.get('notSm?'), 'Should be xs');
+    assert.ok(viewport.get('notMd?'), 'Should be xs');
+    assert.ok(viewport.get('notLg?'), 'Should be xs');
+    assert.notOk(viewport.get('notXs?'), 'Should be xs');
+    assert.notOk(viewport.get('sm?'), 'Should be xs');
+    assert.notOk(viewport.get('md?'), 'Should be xs');
+    assert.notOk(viewport.get('lg?'), 'Should be xs');
+  });
 
-test('when xs?', function (assert) {
-  let ViewportObject = EmberObject.extend(ViewportMixin);
-  const viewport = ViewportObject.create({ '_width': 767 });
-  assert.ok(viewport.get('xs?'), 'Should be xs');
-  assert.ok(viewport.get('notSm?'), 'Should be xs');
-  assert.ok(viewport.get('notMd?'), 'Should be xs');
-  assert.ok(viewport.get('notLg?'), 'Should be xs');
-  assert.notOk(viewport.get('notXs?'), 'Should be xs');
-  assert.notOk(viewport.get('sm?'), 'Should be xs');
-  assert.notOk(viewport.get('md?'), 'Should be xs');
-  assert.notOk(viewport.get('lg?'), 'Should be xs');
-});
+  test('when sm?', function (assert) {
+    let ViewportObject = EmberObject.extend(ViewportMixin);
+    const viewport = ViewportObject.create({ '_width': 768 });
+    assert.ok(viewport.get('notXs?'), 'Should be sm');
+    assert.ok(viewport.get('sm?'), 'Should be sm');
+    assert.ok(viewport.get('notMd?'), 'Should be sm');
+    assert.ok(viewport.get('notLg?'), 'Should be sm');
+    assert.notOk(viewport.get('xs?'), 'Should be sm');
+    assert.notOk(viewport.get('notSm?'), 'Should be sm');
+    assert.notOk(viewport.get('md?'), 'Should be sm');
+    assert.notOk(viewport.get('lg?'), 'Should be sm');
+  });
 
-test('when sm?', function (assert) {
-  let ViewportObject = EmberObject.extend(ViewportMixin);
-  const viewport = ViewportObject.create({ '_width': 768 });
-  assert.ok(viewport.get('notXs?'), 'Should be sm');
-  assert.ok(viewport.get('sm?'), 'Should be sm');
-  assert.ok(viewport.get('notMd?'), 'Should be sm');
-  assert.ok(viewport.get('notLg?'), 'Should be sm');
-  assert.notOk(viewport.get('xs?'), 'Should be sm');
-  assert.notOk(viewport.get('notSm?'), 'Should be sm');
-  assert.notOk(viewport.get('md?'), 'Should be sm');
-  assert.notOk(viewport.get('lg?'), 'Should be sm');
-});
+  test('when md?', function (assert) {
+    let ViewportObject = EmberObject.extend(ViewportMixin);
+    const viewport = ViewportObject.create({ '_width': 992 });
+    assert.ok(viewport.get('notXs?'), 'Should be md');
+    assert.ok(viewport.get('notSm?'), 'Should be md');
+    assert.ok(viewport.get('md?'), 'Should be md');
+    assert.ok(viewport.get('notLg?'), 'Should be md');
+    assert.notOk(viewport.get('xs?'), 'Should be md');
+    assert.notOk(viewport.get('sm?'), 'Should be md');
+    assert.notOk(viewport.get('notMd?'), 'Should be md');
+    assert.notOk(viewport.get('lg?'), 'Should be md');
+  });
 
-test('when md?', function (assert) {
-  let ViewportObject = EmberObject.extend(ViewportMixin);
-  const viewport = ViewportObject.create({ '_width': 992 });
-  assert.ok(viewport.get('notXs?'), 'Should be md');
-  assert.ok(viewport.get('notSm?'), 'Should be md');
-  assert.ok(viewport.get('md?'), 'Should be md');
-  assert.ok(viewport.get('notLg?'), 'Should be md');
-  assert.notOk(viewport.get('xs?'), 'Should be md');
-  assert.notOk(viewport.get('sm?'), 'Should be md');
-  assert.notOk(viewport.get('notMd?'), 'Should be md');
-  assert.notOk(viewport.get('lg?'), 'Should be md');
-});
-
-test('when lg?', function (assert) {
-  let ViewportObject = EmberObject.extend(ViewportMixin);
-  const viewport = ViewportObject.create({ '_width': 1200 });
-  assert.ok(viewport.get('notXs?'), 'Should be lg');
-  assert.ok(viewport.get('notSm?'), 'Should be lg');
-  assert.ok(viewport.get('notMd?'), 'Should be lg');
-  assert.ok(viewport.get('lg?'), 'Should be lg');
-  assert.notOk(viewport.get('xs?'), 'Should be lg');
-  assert.notOk(viewport.get('sm?'), 'Should be lg');
-  assert.notOk(viewport.get('md?'), 'Should be lg');
-  assert.notOk(viewport.get('notLg?'), 'Should be lg');
+  test('when lg?', function (assert) {
+    let ViewportObject = EmberObject.extend(ViewportMixin);
+    const viewport = ViewportObject.create({ '_width': 1200 });
+    assert.ok(viewport.get('notXs?'), 'Should be lg');
+    assert.ok(viewport.get('notSm?'), 'Should be lg');
+    assert.ok(viewport.get('notMd?'), 'Should be lg');
+    assert.ok(viewport.get('lg?'), 'Should be lg');
+    assert.notOk(viewport.get('xs?'), 'Should be lg');
+    assert.notOk(viewport.get('sm?'), 'Should be lg');
+    assert.notOk(viewport.get('md?'), 'Should be lg');
+    assert.notOk(viewport.get('notLg?'), 'Should be lg');
+  });
 });

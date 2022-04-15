161af7a28a51ae384c45b0b7a5443924ef4ad2e6
nadnoslen <dan@cybertooth.io>
2022-04-14 20:34:01 -0600

    :recycle: Codemods For 3.11

--- tests/integration/components/twbs-clearfix-test.js
+++ tests/integration/components/twbs-clearfix-test.js
@@ -9,72 +9,72 @@ module('Integration | Component | twbs clearfix', function(hooks) {
   test('when index is null', async function(assert) {
     this.set('index', null);
 
-    await render(hbs`{{twbs-clearfix columnCount=3 visible-xs=true index=index}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @visible-xs={{true}} @index={{index}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when index is NaN', async function(assert) {
     this.set('index', 'abcd');
-    await render(hbs`{{twbs-clearfix columnCount=3 visible-xs=true index=index}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @visible-xs={{true}} @index={{index}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when index is negative', async function(assert) {
     this.set('index', -1);
-    await render(hbs`{{twbs-clearfix columnCount=3 visible-xs=true index=index}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @visible-xs={{true}} @index={{index}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when columnCount is null', async function(assert) {
     this.set('columnCount', null);
-    await render(hbs`{{twbs-clearfix columnCount=columnCount visible-xs=true index=0}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{columnCount}} @visible-xs={{true}} @index={{0}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when columnCount is NaN', async function(assert) {
     this.set('columnCount', 'abcd');
-    await render(hbs`{{twbs-clearfix columnCount=columnCount visible-xs=true index=0}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{columnCount}} @visible-xs={{true}} @index={{0}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when columnCount is 0', async function(assert) {
     this.set('columnCount', 0);
-    await render(hbs`{{twbs-clearfix columnCount=columnCount visible-xs=true index=0}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{columnCount}} @visible-xs={{true}} @index={{0}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when columnCount is negative', async function(assert) {
     this.set('columnCount', -1);
-    await render(hbs`{{twbs-clearfix columnCount=columnCount visible-xs=true index=0}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{columnCount}} @visible-xs={{true}} @index={{0}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when rendering a clearfix for all sizes every three columns', async function(assert) {
-    await render(hbs`{{twbs-clearfix columnCount=3 index=0 visible-all=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{0}} @visible-all={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=1 visible-all=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{1}} @visible-all={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=2 visible-all=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{2}} @visible-all={{true}} />`);
     assert.dom('div.twbs-clearfix').hasClass('clearfix');
     assert.dom('div.twbs-clearfix').hasClass('visible-lg-block');
     assert.dom('div.twbs-clearfix').hasClass('visible-md-block');
     assert.dom('div.twbs-clearfix').hasClass('visible-sm-block');
     assert.dom('div.twbs-clearfix').hasClass('visible-xs-block');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=3 visible-all=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{3}} @visible-all={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when rendering three columns', async function(assert) {
-    await render(hbs`{{twbs-clearfix columnCount=3 index=0 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{0}} @visible-xs={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=1 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{1}} @visible-xs={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=2 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{2}} @visible-xs={{true}} />`);
     // assert.dom('div.twbs-clearfix').hasAttribute('style', undefined);
     assert.dom('div.twbs-clearfix').hasClass('clearfix');
     assert.dom('div.twbs-clearfix').hasNoClass('visible-lg-block');
@@ -82,18 +82,18 @@ module('Integration | Component | twbs clearfix', function(hooks) {
     assert.dom('div.twbs-clearfix').hasNoClass('visible-sm-block');
     assert.dom('div.twbs-clearfix').hasClass('visible-xs-block');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=3 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{3}} @visible-xs={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when rendering three columns for two different sizes', async function(assert) {
-    await render(hbs`{{twbs-clearfix columnCount=3 index=0 visible-sm=true visible-md=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{0}} @visible-sm={{true}} @visible-md={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=1 visible-sm=true visible-md=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{1}} @visible-sm={{true}} @visible-md={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=2 visible-sm=true visible-md=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{2}} @visible-sm={{true}} @visible-md={{true}} />`);
     assert.dom('div.twbs-clearfix').doesNotHaveAttribute('style');
     assert.dom('div.twbs-clearfix').hasClass('clearfix');
     assert.dom('div.twbs-clearfix').hasNoClass('visible-lg-block');
@@ -101,15 +101,15 @@ module('Integration | Component | twbs clearfix', function(hooks) {
     assert.dom('div.twbs-clearfix').hasClass('visible-sm-block');
     assert.dom('div.twbs-clearfix').hasNoClass('visible-xs-block');
 
-    await render(hbs`{{twbs-clearfix columnCount=3 index=3 visible-sm=true visible-md=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{3}} @index={{3}} @visible-sm={{true}} @visible-md={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
   });
 
   test('when rendering two columns', async function(assert) {
-    await render(hbs`{{twbs-clearfix columnCount=2 index=0 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{2}} @index={{0}} @visible-xs={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=2 index=1 visible-lg=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{2}} @index={{1}} @visible-lg={{true}} />`);
     assert.dom('div.twbs-clearfix').doesNotHaveAttribute('style');
     assert.dom('div.twbs-clearfix').hasClass('clearfix');
     assert.dom('div.twbs-clearfix').hasClass('visible-lg-block');
@@ -117,10 +117,10 @@ module('Integration | Component | twbs clearfix', function(hooks) {
     assert.dom('div.twbs-clearfix').hasNoClass('visible-sm-block');
     assert.dom('div.twbs-clearfix').hasNoClass('visible-xs-block');
 
-    await render(hbs`{{twbs-clearfix columnCount=2 index=2 visible-xs=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{2}} @index={{2}} @visible-xs={{true}} />`);
     assert.equal(find('div.twbs-clearfix').style.display, 'none');
 
-    await render(hbs`{{twbs-clearfix columnCount=2 index=3 visible-md=true}}`);
+    await render(hbs`<TwbsClearfix @columnCount={{2}} @index={{3}} @visible-md={{true}} />`);
     assert.dom('div.twbs-clearfix').doesNotHaveAttribute('style');
     assert.dom('div.twbs-clearfix').hasClass('clearfix');
     assert.dom('div.twbs-clearfix').hasNoClass('visible-lg-block');
